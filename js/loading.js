4d1545d2b63bad1a2fd7e446214f92348b3fa867
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-02-11 21:08:25 +0330

    loading.js

--- js/loading.js
+++ js/loading.js
@@ -1,16 +1,19 @@
 function startLoading() {
-  const loadingEl = document.getElementById('loading');
-  const convertButton = document.querySelector('button[onclick="convertConfig()"]');
-  const clearButton = document.getElementById('clearButton');
-  loadingEl.style.display = 'flex';
-  convertButton.disabled = true;
-  clearButton.disabled = true;
+    const loadingEl = document.getElementById('loading');
+    const convertButton = document.querySelector('button[onclick="convertConfig()"]');
+    const clearButton = document.getElementById('clearButton');
+    
+    loadingEl.style.display = 'flex';
+    convertButton.disabled = true;
+    clearButton.disabled = true;
 }
+
 function stopLoading() {
-  const loadingEl = document.getElementById('loading');
-  const convertButton = document.querySelector('button[onclick="convertConfig()"]');
-  const clearButton = document.getElementById('clearButton');
-  loadingEl.style.display = 'none';
-  convertButton.disabled = false;
-  clearButton.disabled = false;
+    const loadingEl = document.getElementById('loading');
+    const convertButton = document.querySelector('button[onclick="convertConfig()"]');
+    const clearButton = document.getElementById('clearButton');
+    
+    loadingEl.style.display = 'none';
+    convertButton.disabled = false;
+    clearButton.disabled = false;
 }
