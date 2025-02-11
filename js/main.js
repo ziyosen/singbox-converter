90b0d96f4e28d9185e6ac3aa82ae431afff58f65
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-02-11 18:31:14 +0330

    main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ function clearAll() {
     document.getElementById('input').value = '';
     editor.setValue('');
     document.getElementById('error').textContent = '';
-    document.getElementById('clearButton').disabled = true;
 }
 
 function copyToClipboard() {
@@ -23,12 +22,3 @@ function copyToClipboard() {
         .then(() => alert('Configuration copied to clipboard!'))
         .catch(err => console.error('Failed to copy:', err));
 }
-
-document.getElementById('input').addEventListener('input', function() {
-    const clearButton = document.getElementById('clearButton');
-    if (this.value.trim()) {
-        clearButton.disabled = false;
-    } else {
-        clearButton.disabled = true;
-    }
-});
