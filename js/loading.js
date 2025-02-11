380862ff974035f07553cb4a84c877fc65796334
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-02-11 18:30:44 +0330

    loading.js

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ function startLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
-
+    
     loadingEl.style.display = 'flex';
     convertButton.disabled = true;
     clearButton.disabled = true;
@@ -12,7 +12,7 @@ function stopLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
-
+    
     loadingEl.style.display = 'none';
     convertButton.disabled = false;
     clearButton.disabled = false;
