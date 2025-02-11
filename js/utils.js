2ecd8555264c76555d201711c6e63852eb0c594f
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-02-11 19:05:36 +0330

    utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,27 +1,12 @@
-function createTransport(data) {
-    const transport = {};
-    if (data.net && data.net !== 'tcp') {
-        transport.type = data.net;
-        if (data.path) transport.path = data.path;
-        if (data.host) transport.headers = { Host: data.host };
-    }
-    return transport;
-}
-
-function createTransportFromParams(params) {
-    const transport = {};
-    const type = params.get('type');
-    if (type && type !== 'tcp') {
-        transport.type = type;
-        if (params.get('path')) transport.path = params.get('path');
-        if (params.get('host')) transport.headers = { Host: params.get('host') };
-    }
-    return transport;
-}
-
 function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
+    return crypto.randomUUID();
+}
+
+function validateIP(ip) {
+    return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip);
+}
+
+function parsePort(port) {
+    const p = parseInt(port);
+    return p > 0 && p < 65536 ? p : 443;
 }
