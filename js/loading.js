180f58ec740696ad0151d131052d6b4a3e830ec9
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-03-17 23:03:41 +0330

    loading.js

--- js/loading.js
+++ js/loading.js
@@ -2,18 +2,22 @@ function startLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
+    const checkbox = document.getElementById('enableAdBlockAndIran');
     
     loadingEl.style.display = 'flex';
     convertButton.disabled = true;
     clearButton.disabled = true;
+    checkbox.disabled = true;
 }
 
 function stopLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
+    const checkbox = document.getElementById('enableAdBlockAndIran');
     
     loadingEl.style.display = 'none';
     convertButton.disabled = false;
     clearButton.disabled = false;
+    checkbox.disabled = false;
 }
