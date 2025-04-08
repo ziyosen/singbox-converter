03aca3a6e80c81f6a86d05b83bd0858be5214f0f
Anonymous <159781143+4n0nymou3@users.noreply.github.com>
2025-04-08 21:01:01 +0330

    loading.js

--- js/loading.js
+++ js/loading.js
@@ -2,13 +2,15 @@ function startLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
-    const checkbox = document.getElementById('enableAdBlockAndIran');
+    const checkboxAdBlock = document.getElementById('enableAdBlockAndIran');
+    const checkboxCustomTag = document.getElementById('enableCustomTag');
     const pasteButtons = document.querySelectorAll('.terminal-actions button');
 
     loadingEl.style.display = 'flex';
     convertButton.disabled = true;
     clearButton.disabled = true;
-    checkbox.disabled = true;
+    checkboxAdBlock.disabled = true;
+    checkboxCustomTag.disabled = true;
     pasteButtons.forEach(btn => btn.disabled = true);
 }
 
@@ -16,12 +18,14 @@ function stopLoading() {
     const loadingEl = document.getElementById('loading');
     const convertButton = document.querySelector('button[onclick="convertConfig()"]');
     const clearButton = document.getElementById('clearButton');
-    const checkbox = document.getElementById('enableAdBlockAndIran');
+    const checkboxAdBlock = document.getElementById('enableAdBlockAndIran');
+    const checkboxCustomTag = document.getElementById('enableCustomTag');
     const pasteButtons = document.querySelectorAll('.terminal-actions button');
 
     loadingEl.style.display = 'none';
     convertButton.disabled = false;
     clearButton.disabled = false;
-    checkbox.disabled = false;
+    checkboxAdBlock.disabled = false;
+    checkboxCustomTag.disabled = false;
     pasteButtons.forEach(btn => btn.disabled = false);
 }
